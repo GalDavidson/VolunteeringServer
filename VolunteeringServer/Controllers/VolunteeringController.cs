36ac857dc69297b3da5fc12fb1498bcd83014876
User <User@LAB3-6>
2021-09-13 14:34:38 +0300

    Add project files.

---
+++ VolunteeringServer/Controllers/VolunteeringController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VolunteeringServerBL.Models;
+
+namespace VolunteeringServer.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class VolunteeringController : ControllerBase
+    {
+        #region Add connection to the db context using dependency injection
+        volunteeringDBContext context;
+        public VolunteeringController(volunteeringDBContext context)
+        {
+            this.context = context;
+        }
+        #endregion
+    }
+}
