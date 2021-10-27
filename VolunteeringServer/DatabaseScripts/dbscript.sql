e60278da7270b7a42b69aa793188007157142e8d
GalDavidson <77728037+GalDavidson@users.noreply.github.com>
2021-10-27 13:15:16 +0300

    Update dbscript.sql

--- VolunteeringServer/DatabaseScripts/dbscript.sql
+++ VolunteeringServer/DatabaseScripts/dbscript.sql
@@ -1,3 +1,11 @@
+Use master
+CREATE DATABASE VolunteeringDB
+Go
+
+Use VolunteeringDB
+Go
+
+
 CREATE TABLE Gender(
     GenderID INT IDENTITY(1,1) PRIMARY KEY,
     GenderType NVARCHAR NOT NULL
@@ -135,3 +143,199 @@ CREATE TABLE OccupationalAreasOfPosts(
 );
 
 
+
+
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Afula');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Arad');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Ashdod');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Ashkelon');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Baqa al-Gharbiyye');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Bat Yam');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Beersheba');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Beit Shean');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Beit Shemesh');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Bnei Brak');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Dimona');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Eilat');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Elad');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Givat Shmuel');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Givatayim');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Hadera');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Haifa');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Herzliya');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Hod HaSharon');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Holon');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Jerusalem');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Kafr Qasim');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Karmiel');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Kfar Saba');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Kfar Yona');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Kiryat Ata');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Kiryat Bialik');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Kiryat Gat');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Kiryat Malakhi');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Kiryat Motzkin');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Kiryat Ono');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Kiryat Shmona');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Kiryat Yam');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Lod');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Maalot-Tarshiha');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Migdal HaEmek');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Modiin-Maccabim-Reut');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Nahariya');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Nazareth');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Nesher');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Ness Ziona');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Netanya');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Netivot');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Nof HaGalil');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Ofakim');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Or Akiva');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Or Yehuda');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Petah Tikva');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Qalansawe');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Raanana');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Rahat');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Ramat Gan');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Ramat HaSharon');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Ramla');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Rehovot');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Rishon LeZion');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Rosh HaAyin');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Rehovot');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Safed');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Sakhnin');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Sderot');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Shefa-Amr');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Tamra');
+
+INSERT INTO Branches([BranchLocation])
+VALUES ('Tayibe');
+
+Go
